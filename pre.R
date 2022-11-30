49f2cd1895a8ba821d6908a87af5b566eb7ce4b0
devuk <devuk0204@gmail.com>
2022-11-30 18:13:01 +0900

    img update

--- pre.R
+++ pre.R
@@ -1,5 +1,5 @@
 #install.packages('pheatmap')
-install.packages('ggcorrplot')
+#install.packages('ggcorrplot')
 library(pheatmap)
 library(ggplot2)
 library(dplyr)
@@ -57,8 +57,27 @@ p_value2 <- round(cor_pmat(temp), 4)
 ggcorrplot(corr2, method = 'square', type = 'lower', show.legend = F, show.diag = F, outline.color = 'gray',
            lab = F, lab_col = 'black', lab_size = 3.5, p.mat = p_value2, sig.level = 0.05, pch.col = 'black',
            tl.cex = 10, tl.col = 'white', tl.srt = 0, ggtheme = ggplot2::theme_test())
+feature <- c()
+length(corr2)
 
-write.csv(temp[, 1:73], "feature.csv", row.names = F)
-write.csv(temp[, 74], "target.csv", row.names = F)
+for(i in 1:length(corr2[, 1])) {
+  if(abs(corr2[74, i]) >= 0.2) {
+    feature <- c(feature, i)
+  }
+}
+length(feature)
+feature <- feature[-24]
 
-temp
+feature <- temp[, feature]
+target <- temp[, 74]
+
+phishing.df <- cbind(feature, target)
+
+corr3 <- round(cor(phishing.df, use = "all.obs", method = "pearson"), 4)
+p_value3 <- round(cor_pmat(phishing.df), 4)
+ggcorrplot(corr3, method = 'square', type = 'lower', show.legend = F, show.diag = F, outline.color = 'gray',
+           lab = F, lab_col = 'black', lab_size = 3.5, p.mat = p_value3, sig.level = 0.05, pch.col = 'black',
+           tl.cex = 10, tl.col = 'white', tl.srt = 0, ggtheme = ggplot2::theme_test())
+
+#write.csv(temp[, feature], "feature.csv", row.names = F)
+#write.csv(temp[, 74], "target.csv", row.names = F)
